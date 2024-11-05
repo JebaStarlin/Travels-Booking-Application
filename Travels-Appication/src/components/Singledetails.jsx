2cbf93b54b0ec8c55f33fb63c3de3d365e1ce73c
Hirushi kesan <hirushikesan31@gmail.com>
2024-11-05 18:45:59 +0530

    modified:   Travels-Appication/node_modules/.vite/deps/_metadata.json 	modified:   Travels-Appication/src/App.jsx 	modified:   Travels-Appication/src/components/City.jsx 	modified:   Travels-Appication/src/components/Package.jsx 	modified:   Travels-Appication/src/components/Singledetails.jsx

--- Travels-Appication/src/components/Singledetails.jsx
+++ Travels-Appication/src/components/Singledetails.jsx
@@ -12,11 +12,25 @@ function Singledetails() {
             <p>Boarding point:</p>
             <p>Dropping point:</p>
         </div>
-        <div>    
-            <p ><input className="h-[25px] w-[2rem] bg-black text-white" type="text"/> </p>
+        <div className="flex flex-row justify-between ml-20 px-4">    
+            <p ><input className="h-[25px] w-[5rem] bg-black text-white rounded-sm" type="text"/> </p>
+            <p ><input className="h-[25px] w-[5rem] bg-black text-white rounded-sm" type="text"/> </p>
+            <p ><input className="h-[25px] w-[5rem] bg-black text-white rounded-sm" type="text"/> </p>
+            <p ><input className="h-[25px] w-[5rem] bg-black text-white rounded-sm" type="text"/> </p>
+            <p ><input className="h-[25px] w-[5rem] bg-black text-white rounded-sm" type="text"/> </p>
         </div>
+        <hr className="border-black mt-3"/>
         </div>
-      
+        <div className="flex flex-row px-20 justify-between ">
+            <p>Service</p>
+            <p>Bus type</p>
+            <p>Departure:</p>
+            <p>Arrival:</p>
+            <p>No.of.seats:</p>
+        </div>
+        <div>
+          
+        </div>
     </div>
   )
 }
